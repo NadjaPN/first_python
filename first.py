e95463c6944280187f9975aa11082e1b6c5c9494
NadjaPN <pavlova.nadezsda@gmail.com>
2021-12-08 11:44:40 +0100

    Modify hello to hello3.

---
+++ first.py
@@ -0,0 +1,15 @@
+import random
+
+a = random.randint(2, 8) # int(input('Give me a number: '))
+
+# if 9 < a < 100:
+#     print("It's a two-digit number")
+
+# if a % 2 == 0:
+#     print('Even')
+# else:
+#     print('Odd')
+
+for i in range(1, a + 1):
+    print(i)
+
